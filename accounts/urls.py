509f2524169a7a18eaa4f5a3427c39e46824e730
Fahim Muntasir <244419129+fmuntasir751-prog@users.noreply.github.com>
2026-09-07 11:18:26 +0900

    Complete user registration and password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from . import views
 
@@ -36,5 +36,46 @@ urlpatterns = [
         "profile/edit/",
         views.profile_edit,
         name="profile_edit",
+    ),
+
+    # Forgot Password
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="accounts/password_reset_form.html",
+            email_template_name="accounts/password_reset_email.html",
+            subject_template_name="accounts/password_reset_subject.txt",
+            success_url=reverse_lazy(
+                "accounts:password_reset_done"
+            ),
+        ),
+        name="password_reset",
+    ),
+
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="accounts/password_reset_done.html",
+        ),
+        name="password_reset_done",
+    ),
+
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="accounts/password_reset_confirm.html",
+            success_url=reverse_lazy(
+                "accounts:password_reset_complete"
+            ),
+        ),
+        name="password_reset_confirm",
+    ),
+
+    path(
+        "reset/complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="accounts/password_reset_complete.html",
+        ),
+        name="password_reset_complete",
     ),
 ]
